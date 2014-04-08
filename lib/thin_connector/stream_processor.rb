3ccb2a4e1130fd0c06f97a03999d2fc362bbce0e
Nick Isaacs <nisaacs@splickit.com>
2014-04-08 17:42:01 -0600

    refactor processor

--- lib/thin_connector/stream_processor.rb
+++ lib/thin_connector/stream_processor.rb
@@ -3,12 +3,17 @@
 # it merely places the stream contents into a redis queue for processing
 # by other actors
 
+require 'redis'
 module ThinConnector
   class StreamProcessor
 
     REDIS_NAMESPACE = "stream_processor:raw"
 
-    def initialize(stream)
+    def initialize(the_stream)
+      @stream = the_stream
+    end
+
+    def start
       stream.start do |object|
         put_in_redis object
       end
@@ -17,13 +22,14 @@ module ThinConnector
     private
 
     def put_in_redis(obj)
-
+      redis_client.lpush redis_queue, obj
     end
 
     def redis_queue; Environment.instance.redis_namespace + ":#{REDIS_NAMESPACE}"; end
 
     def redis_client
-
+      @_redis_client ||= Redis.new ThinConnector::Environment.instance.redis_config
+      @_redis_client
     end
 
     def stream; @stream; end
