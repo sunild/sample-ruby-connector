4006d56c8d1be258d0b4dff0664883d65b7f8013
Nick Isaacs <nisaacs@splickit.com>
2014-04-08 08:16:54 -0600

    impliment own singleton pattern

--- lib/thin_connector/environment.rb
+++ lib/thin_connector/environment.rb
@@ -1,14 +1,26 @@
-require 'singleton'
 require 'yaml'
 
 module ThinConnector
   class Environment
-    include Singleton
 
     REDIS_NAMESPACE = 'thinconnector'
-    attr_reader :gnip_username, :gnip_password, :root, :redis_config, :env
+    DEFAULT_ENV = 'development'
 
-    load_project_configuration
+    attr_reader :gnip_username, :gnip_password, :root, :redis_config, :log_level
+    @@_singleton__instance = nil
+    @@_singleton_mutex = Mutex.new
+
+    def self.instance
+      #raise "No environment configuration passed into nil singleton" if !env && !@@_singleton__instance
+      return @@_singleton__instance if @@_singleton__instance
+      @@_singleton_mutex.synchronize {
+        return @@_singleton__instance if @@_singleton__instance
+        @@_singleton__instance = new
+        @@_singleton__instance.load_project_configuration
+      }
+      @@_singleton__instance
+    end
+
 
     def env=(set_env)
       if env
@@ -18,7 +30,25 @@ module ThinConnector
       end
     end
 
+    def env
+      @env || DEFAULT_ENV
+    end
+
+    def load_project_configuration
+      config = YAML.load_file(configuration_file_path)[env]
+
+      @gnip_username = config['gnip_username']
+      @gnip_password = config['gnip_password']
+      @gnip_url      = config['gnip_url']
+      @root          = File.join File.expand_path(File.dirname __FILE__ ), '..', '..'
+      @log_level     = config['log_level']
+    end
+
     private
+
+    def initialize
+      load_project_configuration
+    end
 
     def configuration_file_path
       File.join File.expand_path( File.dirname(__FILE__) ), '..', '..', 'config', 'application.yml'
@@ -26,13 +56,6 @@ module ThinConnector
 
     def redis_configuration_file_path
       File.join File.expand_path( File.dirname(__FILE__) ), '..', '..', 'config', 'redis.yml'
-    end
-
-    def load_project_configuration
-      config = YAML.load_file configuration_file_path
-      @gnip_username = config['gnip_username']
-      @gnip_password = config['gnip_password']
-      @root = File.join File.expand_path(File.dirname __FILE__ ), '..', '..'
     end
 
     def load_redis_configuration
