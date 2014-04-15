b7fff7df78657e0f143f18f375438f364e2e112e
Nick Isaacs <nisaacs@gnip.com>
2014-04-15 11:45:42 -0600

    add specs

---
+++ spec/spec_helper.rb
@@ -0,0 +1,8 @@
+require 'bundler/setup'
+Bundler.setup
+
+require_relative '../lib/thin_connector.rb' # and any other gems you need
+
+RSpec.configure do |config|
+
+end
---
+++ spec/stream/gnip_stream_spec.rb
@@ -0,0 +1,13 @@
+require '../spec_helper'
+
+describe GnipStream do
+
+  let(:stream)  { GnipStream.new(ThinConnector::Environment.gnip_url) }
+  it 'should reconnect after being inturupted' do
+
+  end
+
+  it 'should start the stream and execute the block passed in' do
+
+  end
+end
